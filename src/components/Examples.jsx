c73eb746e061bf9e2b11aefd3b4e19bec9db1f07
rajasekharzdev <rajasekharmdev@gmail.com>
2024-07-01 20:45:30 -0400

    react essentials

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -26,6 +26,7 @@ export default function Examples() {
     <>
       <Section title="Examples" id="examples">
         <Tabs
+          buttonsContainer="menu"
           buttons={
             <div>
               <TabButton
